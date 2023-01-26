1d2055caee48680b867de392e747504994eaf2d9
RenoSimonsWebit <simons.reno@gmail.com>
2023-01-26 16:37:48 +0100

    Extended handler

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -2,16 +2,30 @@
 
 namespace webit_be\developer_alert\Exceptions;
 
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as BaseHandler;
 use Illuminate\Support\Facades\Log;
-use Throwable;
+use webit_be\developer_alert\Models\Alert;
+
 class Handler extends BaseHandler
 {
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            Log::info('called from throwable');
-            Log::info($e);
+
+            // Report the alert in the alerts table
+
+            if (Alert::where('message', $e->getMessage())->where('where_from', $e->getFile())->exists()) {
+                $alert = Alert::create([
+                    'error_message' => $e->getMessage(),
+                    'where_from' => $e->getFile(),
+                    'from_handler_extension' => 1,
+                ]);
+            } else {
+                $alert = Alert::where('error_message', $e->getMessage())->where('where_from', $e->getFile())->first();
+                $alert->times_throwed = $alert->times_throwed += 1;
+                $alert->save();
+            }
         });
-    }    
+    }
 }
