a970109f1d34f0b20e3366532c8bf07dad4821cf
RenoSimonsWebit <simons.reno@gmail.com>
2023-01-26 16:10:12 +0100

    Check if throwable works

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -4,11 +4,14 @@ namespace webit_be\developer_alert\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as BaseHandler;
 use Illuminate\Support\Facades\Log;
-
+use Throwable;
 class Handler extends BaseHandler
 {
     public function register()
     {
-        Log::info('register called in package');
+        $this->reportable(function (Throwable $e) {
+            Log::info('called from throwable');
+            Log::info($e);
+        });
     }    
 }
